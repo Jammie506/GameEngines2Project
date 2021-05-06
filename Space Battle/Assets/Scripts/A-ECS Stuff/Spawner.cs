9c1d8f3f1edd072b522990396eafd7adb5640891
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2021-05-06 20:20:32 +0100

    .

---
+++ Space Battle/Assets/Scripts/A-ECS Stuff/Spawner.cs
@@ -0,0 +1,36 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[AddComponentMenu("DOTS Samples/SpawnFromMonoBehaviour/Spawner")]
+public class Spawner : MonoBehaviour
+{
+    public GameObject Prefab;
+    
+    [SerializeField] private float maxX;
+    [SerializeField] private float maxY;
+    [SerializeField] private float maxZ;
+
+    [SerializeField] private float shipCount;
+
+    void Start()
+    {
+        // Create entity prefab from the game object hierarchy once
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, settings);
+        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        for (var y = 0; y < shipCount; y++)
+        {
+            // Efficiently instantiate a bunch of entities from the already converted entity prefab
+            var instance = entityManager.Instantiate(prefab);
+
+            // Place the instantiated entity in a grid with some noise
+            var position = new Vector3(Random.Range(-maxX, maxX), Random.Range(-maxY, maxY), Random.Range(-maxZ, maxZ));
+            var rotation = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), -1);
+            entityManager.SetComponentData(instance, new Translation {Value = position});
+        }
+    }
+}
