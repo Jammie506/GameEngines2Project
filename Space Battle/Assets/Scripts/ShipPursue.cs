b4293c583cafd546c22d352e983993be51bc9d4d
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2021-05-08 17:06:05 +0100

    Add Battle

--- Space Battle/Assets/Scripts/ShipPursue.cs
+++ Space Battle/Assets/Scripts/ShipPursue.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine;
 public class ShipPursue : MainBrain
 {
     public ShipMain target;
-
+    
     Vector3 targetPos;
 
     public void OnDrawGizmos()
